a498b677e903554c2f35d694fd2b6089158c9a4a
Marcelo Bruno <mbrunorp@gmail.com>
2023-04-04 20:00:46 -0300

    fix variable name

--- exercicios/lista_exercicios_03/exercicio_03.py
+++ exercicios/lista_exercicios_03/exercicio_03.py
@@ -1,15 +1,15 @@
 """Escreva um programa que leia 3 números inteiros, determine e mostre o maior deles."""
 
-num_1 = int(input("Digite um número inteiro: "))
-num_2 = int(input("Digite outro número inteiro: "))
-num_3 = int(input("Digite mais um número inteiro: "))
+valor_1 = int(input("Digite um número inteiro: "))
+valor_2 = int(input("Digite outro número inteiro: "))
+valor_3 = int(input("Digite mais um número inteiro: "))
 
 # Using if ... elif ... else structure
-if num_1 > num_2 and num_1 > num_3:
-    print(f'Este é o maior número informado {num_1}')
+if valor_1 > valor_2 and valor_1 > valor_3:
+    print(f'Este é o maior número informado {valor_1}')
 
-elif num_2 > num_1 and num_2 > num_3:
-    print(f'Este é o maior número informado {num_2}')
+elif valor_2 > valor_1 and valor_2 > valor_3:
+    print(f'Este é o maior número informado {valor_2}')
 
 else:
-    print(f'Este é o maior número informado {num_3}')
+    print(f'Este é o maior número informado {valor_3}')
